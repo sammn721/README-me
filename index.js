c3fe2b518b8eb699617c9a9fe109112bdc0d432f
Sam Nielsen <sammn721@gmail.com>
2021-07-11 20:06:02 -0700

    added require lines

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [];
