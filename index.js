3c95de4ad4ea2f6310670b16168f293b473c6ad8
Sam Nielsen <sammn721@gmail.com>
2021-07-11 20:27:52 -0700

    added starting questions

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Give a brief description of your project (< 30 characters):",
+        name: "brief"
+    },
+    {
+        type: "input",
+        message: "Provide instructions for use:",
+        name: "instructions"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
