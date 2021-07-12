880d18139d34f7b85a92a39443f1ec8dcee782e0
Sam Nielsen <sammn721@gmail.com>
2021-07-11 23:52:14 -0700

    added some text lines to generate

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,15 +4,36 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
+
+function renderTitle(title) {
+  return `# ${title}`
+}
+
+function renderBrief(brief) {
+  return `# ${brief}`
+}
+
+function renderInstructions(instructions) {
+  return `# ${instructions}`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `${renderTitle(data.title)}
+  \n${renderBrief(data.brief)}
+  \n${renderInstructions(data.instructions)}
+  \n${renderLicenseBadge(data.license)}
+  \n${renderLicenseLink(data.license)}
+  \n${renderLicenseSection(data.license)}
 
 `;
 }
