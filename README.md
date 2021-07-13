22ab735a3adbf475612d703c4e26d2b9feaf76f0
Sam Nielsen <sammn721@gmail.com>
2021-07-12 23:08:21 -0700

    added some text to my readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # README-me
 A README file generator
+
+## Description 
+The goal of this project is to write a program tto generate README files more detailed than any I've made up to this point.
+
+## Installation/Usage
+Video demo to go here.
+
+https://github.com/sammn721/README-me
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![${license} license](https://img.shields.io/badge/License-${license}-informational)`;
+  } else if (license === "none") {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
