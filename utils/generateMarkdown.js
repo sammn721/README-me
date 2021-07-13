621ccd11c3c0cb91cfe68932f2467dc6a9e9725e
Sam Nielsen <sammn721@gmail.com>
2021-07-13 01:16:46 -0700

    wrote switch statement for rendering license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,23 +2,34 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch(license) {
-    case 'Apache-2.0':
-      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
-    case 'MIT':
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
-    case 'GPLv2':
-      return '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]';
-    case 'GPLv3':
-      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
-    case 'No license':
-      return ''
+    case "Apache-2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+    case "GPLv2":
+      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]";
+    case "GPLv3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+    case "No license":
+      return ""
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  switch(license) {
+    case "Apache-2.0":
+      return "(https://opensource.org/licenses/Apache-2.0)";
+    case "MIT":
+      return "(https://opensource.org/licenses/MIT)";
+    case "GPLv3":
+      return "(https://www.gnu.org/licenses/gpl-3.0)";
+    case "GPLv2":
+      return "(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+    case "No license":
+      return "";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
